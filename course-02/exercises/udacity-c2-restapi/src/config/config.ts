334bcef0bab6b822c13538c089f01d13bc023f3b
Başar Beykoz <basar.beykoz@gmail.com>
2020-05-19 22:19:45 +0300

    add auth functions

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,19 +1,17 @@
 export const config = {
-  "dev": {
+  "postgress": {
     "username": process.env.POSTGRESS_USERNAME,
     "password": process.env.POSTGRESS_PASSWORD,
     "database": process.env.POSTGRESS_DATABASE,
     "host": process.env.POSTGRESS_HOST,
-    "dialect": "postgres",
+    "dialect": "postgres"
+  },
+  "aws": {
     "aws_region": process.env.AWS_REGION,
     "aws_profile": process.env.AWS_PROFILE,
     "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
   },
-  "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
-    "dialect": "postgres"
+  "jwt": {
+    "secret": "lOMi_Znya4jkNgoR4v0hk5aiV6wpVXPjU7-JNdjJ3_LXPBKirk9npFgGiEh335g0TYwmNVblykw-DcUD-GBxuYItn2pV4x1OJ4HnemylU6IzP2hBNV0jFxJItAlnRaLo5mQewjQVnWqwo3blyElH5NktTCBXXbjtgZeUmseW0vk"
   }
 }
